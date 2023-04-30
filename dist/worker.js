190e5ed38772888e5acb1de7bbba5134c31aa8b2
Rob Taylor <roboncode@gmail.com>
2023-04-30 07:24:41 -0600

    updated build

--- dist/worker.js
+++ dist/worker.js
@@ -1,14 +1,14 @@
-var F = Object.defineProperty;
-var $ = (e, t, E) => t in e ? F(e, t, { enumerable: true, configurable: true, writable: true, value: E }) : e[t] = E;
-var N = (e, t, E) => ($(e, typeof t != "symbol" ? t + "" : t, E), E), K = (e, t, E) => {
+var $ = Object.defineProperty;
+var Y = (e, t, E) => t in e ? $(e, t, { enumerable: true, configurable: true, writable: true, value: E }) : e[t] = E;
+var S = (e, t, E) => (Y(e, typeof t != "symbol" ? t + "" : t, E), E), D = (e, t, E) => {
   if (!t.has(e))
     throw TypeError("Cannot " + E);
 };
-var S = (e, t, E) => (K(e, t, "read from private field"), E ? E.call(e) : t.get(e)), O = (e, t, E) => {
+var N = (e, t, E) => (D(e, t, "read from private field"), E ? E.call(e) : t.get(e)), O = (e, t, E) => {
   if (t.has(e))
     throw TypeError("Cannot add the same private member more than once");
   t instanceof WeakSet ? t.add(e) : t.set(e, E);
-}, l = (e, t, E, s) => (K(e, t, "write to private field"), s ? s.call(e, E) : t.set(e, E), E);
+}, l = (e, t, E, s) => (D(e, t, "write to private field"), s ? s.call(e, E) : t.set(e, E), E);
 function n() {
   return {
     COOKIE_NAMESPACE: "cookie",
@@ -27,8 +27,8 @@ function n() {
     OFF: "off"
   };
 }
-const B = n().OFF;
-function o(e) {
+const k = n().OFF;
+function i(e) {
   return {
     WINDOW: {
       // Methods
@@ -105,23 +105,26 @@ function o(e) {
     }
   };
 }
-const _ = n(), Y = o(_).WINDOW;
-o(_).NETWORK;
-o(_).STORE;
-o(_).ELEMENT;
-o(_).ELEMENTS;
-o(_).FORM;
-o(_).INPUT;
-const W = o(_).CONNECTION;
-const T = () => ({
+const T = n();
+i(T).WINDOW;
+i(T).NETWORK;
+i(T).STORE;
+i(T).ELEMENT;
+i(T).ELEMENTS;
+i(T).FORM;
+i(T).INPUT;
+const v = i(T).CONNECTION;
+const A = () => ({
   SYSTEM: {
     // Methods
     ECHO: "echo",
-    REQUEST_PERMISSIONS: "requestPermissions"
+    REQUEST_PERMISSIONS: "requestPermissions",
+    SEND_MESSAGE: "sendMessage",
+    OPEN_APP: "openApp",
+    // Events
+    ON_MESSAGE: "message"
   },
   SESSION: {
-    // Methods
-    SET_APP_USER: "setAppUser",
     GET_SESSION: "getSession",
     // Events
     ON_CHANGE: "sessionChange"
@@ -131,8 +134,7 @@ const T = () => ({
     MAXIMIZE: "maximize",
     MINIMIZE: "minimize",
     RESTORE: "restore",
-    SHOW_BADGE: "showBadge",
-    HIDE_BADGE: "hideBadge",
+    SET_BADGE: "setBadge",
     CLOSE: "closeApp",
     // Events
     ON_MAXIMIZE: "maximize",
@@ -144,10 +146,18 @@ const T = () => ({
     QUERY: "QUERY"
     // TODO: move to beam-client?
   },
+  USER: {
+    // Methods
+    // GET_USER: 'getUser',
+    SET_USER: "setUser",
+    // Events
+    ON_CHANGE: "userChange"
+  },
   RESOURCE: {
     // Methods
-    GET_RESOURCE: "getResource",
+    // GET_RESOURCE: 'getResource',
     SET_RESOURCE: "setResource",
+    CLEAR_RESOURCE: "clearResource",
     // Events
     ON_CHANGE: "resourceChange"
   },
@@ -156,8 +166,7 @@ const T = () => ({
     PUSH: "pushNotification",
     CLEAR: "clearNotifications",
     // Events
-    ON_CLICK: "notificationClick",
-    ON_CLEAR: "notificationClear"
+    ON_CLICK: "notificationClick"
   },
   STORE: {
     // Methods
@@ -165,116 +174,118 @@ const T = () => ({
     GET_ITEM: "getStoreItem",
     REMOVE_ITEM: "removeStoreItem"
   }
-}), p = T().SYSTEM, x = T().SESSION, u = T().APP, v = T().ELEMENT, f = T().RESOURCE, h = T().NOTIFICATION, m = T().STORE, ne = self, D = !!ne.WorkerGlobalScope, Oe = D || `${self.name ?? ""}`.startsWith("portal"), H = "0.3.2";
-var g, i, I, R, L, G, C, U, d;
-class q {
+}), a = A().SYSTEM, w = A().SESSION, R = A().APP, y = A().ELEMENT, x = A().RESOURCE, m = A().NOTIFICATION, p = A().STORE, F = A().USER, Oe = self, K = !!Oe.WorkerGlobalScope, oe = K || `${self.name ?? ""}`.startsWith("portal"), q = "0.3.4";
+var g, o, I, P, L, G, _, U, d;
+class H {
   /**
    *
    * @param options Allows to set the request timeout in milliseconds. Default is 0 (no timeout).
    */
   constructor(t, E, s) {
-    N(this, "id");
+    S(this, "id");
     O(this, g, null);
-    O(this, i, []);
+    O(this, o, []);
     O(this, I, /* @__PURE__ */ new Map());
-    O(this, R, /* @__PURE__ */ new Map());
+    O(this, P, /* @__PURE__ */ new Map());
     O(this, L, 0);
     O(this, G, "*");
-    O(this, C, void 0);
-    N(this, "uuid", () => "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(t2) {
+    O(this, _, void 0);
+    S(this, "openApp", (t2, E2) => this.sendRequest(a.OPEN_APP, { appId: t2, message: E2 }));
+    S(this, "uuid", () => "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(t2) {
       var E2 = Math.random() * 16 | 0, s2 = t2 == "x" ? E2 : E2 & 3 | 8;
       return s2.toString(16);
     }));
-    O(this, U, (t2, E2, s2 = S(this, L)) => new Promise((M, A) => {
+    O(this, U, (t2, E2, s2 = N(this, L)) => new Promise((M, C) => {
       let c = null;
       s2 && (c = setTimeout(() => {
-        A(new Error("timeout"));
-      }, s2)), S(this, I).set(t2, { path: E2, timer: c, resolve: M, reject: A });
+        C(new Error("timeout"));
+      }, s2)), N(this, I).set(t2, { path: E2, timer: c, resolve: M, reject: C });
     }));
     O(this, d, async (t2) => {
-      if (S(this, i).push(t2), clearTimeout(S(this, g)), S(this, i).length >= 10) {
-        this.postMessage({ batch: S(this, i) }), S(this, i).length = 0;
+      if (N(this, o).push(t2), clearTimeout(N(this, g)), N(this, o).length >= 10) {
+        this.postMessage({ batch: N(this, o) }), N(this, o).length = 0;
         return;
       }
       l(this, g, setTimeout(() => {
-        this.postMessage({ batch: S(this, i) }), S(this, i).length = 0;
+        this.postMessage({ batch: N(this, o) }), N(this, o).length = 0;
       }, 1));
     });
-    N(this, "postMessage", (t2) => {
-      D ? S(this, C).postMessage(t2) : Oe && S(this, C).postMessage(t2, S(this, G));
+    S(this, "postMessage", (t2) => {
+      K ? N(this, _).postMessage(t2) : oe && N(this, _).postMessage(t2, N(this, G));
     });
-    N(this, "setContext", (t2) => {
-      l(this, C, t2);
+    S(this, "setContext", (t2) => {
+      l(this, _, t2);
     });
-    N(this, "sendRequest", async (t2, ...E2) => {
+    S(this, "sendRequest", async (t2, ...E2) => {
       const s2 = this.uuid();
-      return S(this, d).call(this, { id: s2, path: t2, version: H, args: E2 }), S(this, U).call(this, s2, t2);
+      return N(this, d).call(this, { id: s2, path: t2, version: q, args: E2 }), N(this, U).call(this, s2, t2);
     });
-    N(this, "subscribe", (t2, E2 = "*", s2) => {
+    S(this, "subscribe", (t2, E2 = "*", s2) => {
       const r = this.uuid();
-      return S(this, R).set(r, s2), S(this, d).call(this, { id: r, version: H, path: "on." + t2, args: [E2] }), () => {
-        S(this, R).delete(r), setTimeout(() => {
-          this.postMessage({ id: r, path: B + "." + t2, args: [E2] });
+      return N(this, P).set(r, s2), N(this, d).call(this, { id: r, version: q, path: "on." + t2, args: [E2] }), () => {
+        N(this, P).delete(r), setTimeout(() => {
+          this.postMessage({ id: r, path: k + "." + t2, args: [E2] });
         }, 0);
       };
     });
-    N(this, "disconnect", () => {
-      S(this, C) && this.postMessage({ id: this.id, path: W.DISCONNECT });
+    S(this, "disconnect", () => {
+      N(this, _) && this.postMessage({ id: this.id, path: v.DISCONNECT });
     });
-    N(this, "echo", (t2) => this.sendRequest(p.ECHO, t2));
-    N(this, "requestPermissions", (t2) => this.sendRequest(p.REQUEST_PERMISSIONS, t2));
-    N(this, "getSession", async () => this.sendRequest(x.GET_SESSION));
-    N(this, "getResource", (t2) => this.sendRequest(f.GET_RESOURCE, t2));
-    N(this, "showBadge", (t2) => this.sendRequest(u.SHOW_BADGE, t2));
-    N(this, "hideBadge", () => this.sendRequest(u.HIDE_BADGE, { count: 0 }));
-    N(this, "closeApp", () => this.sendRequest(u.CLOSE));
-    N(this, "pushNotification", (t2) => this.sendRequest(h.PUSH, t2));
-    N(this, "clearNotifications", (t2) => this.sendRequest(h.CLEAR, t2));
-    N(this, "setStoreItem", (t2, E2, s2) => this.sendRequest(m.SET_ITEM, t2, E2, s2));
-    N(this, "getStoreItem", async (t2, E2, s2) => this.sendRequest(m.GET_ITEM, t2, s2).then((r) => r ?? E2 ? E2 : r));
-    N(this, "removeStoreItem", (t2, E2) => this.sendRequest(m.REMOVE_ITEM, t2, E2));
-    N(this, "onSessionChange", (t2) => this.subscribe(x.ON_CHANGE, "*", t2));
-    N(this, "onMaximize", (t2) => this.subscribe(u.ON_MAXIMIZE, "*", t2));
-    N(this, "onMinimize", (t2) => this.subscribe(u.MINIMIZE, "*", t2));
-    N(this, "onRestore", (t2) => this.subscribe(u.RESTORE, "*", t2));
-    N(this, "onResourceChange", (t2 = "*", E2) => this.subscribe(f.ON_CHANGE, t2, E2));
-    N(this, "onNotificationClick", (t2 = "*", E2) => this.subscribe(h.ON_CLICK, t2, E2));
-    N(this, "onNotificationClear", (t2 = "*", E2) => this.subscribe(h.ON_CLEAR, t2, E2));
-    s && l(this, L, s.requestTimeout || 0), this.id = this.uuid(), l(this, C, t), E.addEventListener("message", (r) => {
-      const { id: M, data: A, error: c } = r.data, a = S(this, I).get(M);
-      if (a)
-        a.timer && clearTimeout(a.timer), S(this, I).delete(M), c ? a.reject(c) : a.resolve(A);
+    S(this, "echo", (t2) => this.sendRequest(a.ECHO, t2));
+    S(this, "requestPermissions", (t2) => this.sendRequest(a.REQUEST_PERMISSIONS, t2));
+    S(this, "getSession", async () => this.sendRequest(w.GET_SESSION));
+    S(this, "setBadge", (t2) => this.sendRequest(R.SET_BADGE, t2));
+    S(this, "closeApp", () => this.sendRequest(R.CLOSE));
+    S(this, "pushNotification", (t2) => this.sendRequest(m.PUSH, t2));
+    S(this, "clearNotifications", (t2) => this.sendRequest(m.CLEAR, t2));
+    S(this, "setStoreItem", (t2, E2, s2) => this.sendRequest(p.SET_ITEM, t2, E2, s2));
+    S(this, "getStoreItem", async (t2, E2, s2) => this.sendRequest(p.GET_ITEM, t2, s2).then((r) => r ?? E2 ? E2 : r));
+    S(this, "removeStoreItem", (t2, E2) => this.sendRequest(p.REMOVE_ITEM, t2, E2));
+    S(this, "onSessionChange", (t2) => this.subscribe(w.ON_CHANGE, "*", t2));
+    S(this, "onMaximize", (t2) => this.subscribe(R.ON_MAXIMIZE, "*", t2));
+    S(this, "onMinimize", (t2) => this.subscribe(R.MINIMIZE, "*", t2));
+    S(this, "onRestore", (t2) => this.subscribe(R.RESTORE, "*", t2));
+    S(this, "onResourceChange", (t2 = "*", E2) => this.subscribe(x.ON_CHANGE, t2, E2));
+    S(this, "onMessage", (t2) => this.subscribe(m.ON_CLICK, "*", t2));
+    S(this, "onUserChange", (t2) => () => this.subscribe(F.ON_CHANGE, "*", t2));
+    s && l(this, L, s.requestTimeout || 0), this.id = this.uuid(), l(this, _, t), E.addEventListener("message", (r) => {
+      const { id: M, data: C, error: c } = r.data, u = N(this, I).get(M);
+      if (u)
+        u.timer && clearTimeout(u.timer), N(this, I).delete(M), c ? u.reject(c) : u.resolve(C);
       else {
-        const P = S(this, R).get(M);
-        P && P(A);
+        const h = N(this, P).get(M);
+        h && h(C);
       }
     });
   }
 }
-g = /* @__PURE__ */ new WeakMap(), i = /* @__PURE__ */ new WeakMap(), I = /* @__PURE__ */ new WeakMap(), R = /* @__PURE__ */ new WeakMap(), L = /* @__PURE__ */ new WeakMap(), G = /* @__PURE__ */ new WeakMap(), C = /* @__PURE__ */ new WeakMap(), U = /* @__PURE__ */ new WeakMap(), d = /* @__PURE__ */ new WeakMap();
-class ie extends q {
+g = /* @__PURE__ */ new WeakMap(), o = /* @__PURE__ */ new WeakMap(), I = /* @__PURE__ */ new WeakMap(), P = /* @__PURE__ */ new WeakMap(), L = /* @__PURE__ */ new WeakMap(), G = /* @__PURE__ */ new WeakMap(), _ = /* @__PURE__ */ new WeakMap(), U = /* @__PURE__ */ new WeakMap(), d = /* @__PURE__ */ new WeakMap();
+class ie extends H {
   constructor(E, s) {
     super(E, E, s);
-    N(this, "query", async (E2) => {
+    S(this, "query", async (E2) => {
       const s2 = typeof E2 == "string" ? E2 : E2.toString();
-      return await this.sendRequest(v.QUERY, s2);
+      return await this.sendRequest(y.QUERY, s2);
     });
-    N(this, "$", (E2) => {
+    S(this, "$", (E2) => {
       const s2 = new Proxy(() => {
       }, {});
       return this.generatePathFromProxy(s2).$(E2);
     });
-    N(this, "setResource", (E2) => this.sendRequest(f.SET_RESOURCE, E2));
-    N(this, "setAppUser", (E2) => this.sendRequest(x.SET_APP_USER, E2));
-    N(this, "generatePathFromProxy", (E2) => {
+    S(this, "openApp", (E2, s2) => this.sendRequest(a.OPEN_APP, { appId: E2, message: s2 }));
+    S(this, "setResource", (E2) => this.sendRequest(x.SET_RESOURCE, E2));
+    S(this, "clearResource", (E2) => this.sendRequest(x.CLEAR_RESOURCE, E2));
+    S(this, "setUser", (E2) => this.sendRequest(F.SET_USER, E2));
+    S(this, "sendMessage", (E2) => this.sendRequest(a.SEND_MESSAGE, E2));
+    S(this, "generatePathFromProxy", (E2) => {
       const s2 = [], r = {
-        get(M, A) {
-          return s2.push(A.toString()), new Proxy(() => {
+        get(M, C) {
+          return s2.push(C.toString()), new Proxy(() => {
           }, r);
         },
-        apply(M, A, c) {
-          const a = c.map((y) => JSON.stringify(y)).join(", "), P = s2[s2.length - 1];
-          return P === "toString" ? s2.slice(0, -1).join(".") : (s2[s2.length - 1] = `${P}(${a})`, new Proxy(() => {
+        apply(M, C, c) {
+          const u = c.map((W) => JSON.stringify(W)).join(", "), h = s2[s2.length - 1];
+          return h === "toString" ? s2.slice(0, -1).join(".") : (s2[s2.length - 1] = `${h}(${u})`, new Proxy(() => {
           }, r));
         }
         // Add a valueOf() method to return the string path when the proxy is coerced to a primitive value
@@ -285,27 +296,30 @@ class ie extends q {
       return new Proxy(() => {
       }, r);
     });
-    N(this, "requestPermissions", (E2) => this.sendRequest(p.REQUEST_PERMISSIONS, E2));
+    S(this, "requestPermissions", (E2) => this.sendRequest(a.REQUEST_PERMISSIONS, E2));
   }
 }
-let w;
-const Ce = () => {
-  if (!D)
+let b;
+const _e = () => {
+  if (!K)
     throw new Error("usePortalService must be called from a web worker");
-  return w || (w = new ie(self)), w;
-}, ce = Y;
+  return b || (b = new ie(self)), b;
+}, Me = R;
 async function main() {
-  try {
-    const portal = Ce();
-    const response = await portal.echo("Hello, world!");
-    console.log(response);
-    portal.subscribe(ce.ON_URL_CHANGE, "*", (url) => {
-      if (!url)
-        return;
-      console.log("URL CHANGED:", url);
-    });
-  } catch (error) {
-    console.error(error);
-  }
+  const portal = _e();
+  const badge = {
+    show: true,
+    text: "2",
+    color: "#f204d9"
+  };
+  portal.sendRequest(Me.SET_BADGE, badge);
+  portal.pushNotification({
+    title: "Please follow up with Nancy",
+    text: "Nancy needs a follow up appointment.",
+    data: {
+      patientId: "6"
+    }
+  });
 }
+main();
 main();
--- src/lib/worker.ts
+++ src/lib/worker.ts
@@ -1,17 +1,28 @@
-import { WINDOW, usePortalService } from '@meetportal/portal-js'
+import { APP, Badge, usePortalService } from '@meetportal/portal-js'
 
 export default async function main() {
-  try {
-    const portal = usePortalService()
+  const portal = usePortalService()
 
-    const response = await portal.echo('Hello, world!')
-    console.log(response)
+  // console.log(portal.$('<div id="hi">hello</div>').querySelector('#hi')?.innerHTML.toString())
 
-    portal.subscribe(WINDOW.ON_URL_CHANGE, '*', (url: string) => {
-      if (!url) return
-      console.log('URL CHANGED:', url)
-    })
-  } catch (error: any) {
-    console.error(error)
+  // portal.subscribe(WINDOW.ON_URL_CHANGE, 'test', (url: string) => {
+  //   console.log('🤷‍♀️🤷‍♀️🤷‍♀️🤷‍♀️ url', url)
+  // })
+
+  const badge: Badge = {
+    show: true,
+    text: '2',
+    color: '#f204d9',
   }
+  portal.sendRequest(APP.SET_BADGE, badge)
+
+  portal.pushNotification({
+    title: 'Please follow up with Nancy',
+    text: 'Nancy needs a follow up appointment.',
+    data: {
+      patientId: '6',
+    },
+  })
 }
+
+main()
