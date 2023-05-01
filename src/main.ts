49999fb091ca0badeb358b816caeee87c41751d6
Rob Taylor <roboncode@gmail.com>
2023-04-30 22:31:01 -0600

    updated deps; added prettier; minified; iife

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,27 @@
-import main from './lib/worker'
-main()
+import { APP, Badge, usePortalService } from '@meetportal/portal-js'
+
+const portal = usePortalService()
+
+// console.log(portal.$('<div id="hi">hello</div>').querySelector('#hi')?.innerHTML.toString())
+
+// portal.subscribe(WINDOW.ON_URL_CHANGE, 'test', (url: string) => {
+//   console.log('🤷‍♀️🤷‍♀️🤷‍♀️🤷‍♀️ url', url)
+// })
+
+const badge: Badge = {
+  show: true,
+  text: '2',
+  color: '#f204d9',
+}
+portal.sendRequest(APP.SET_BADGE, badge)
+
+portal.pushNotification({
+  appId: '5',
+  type: 'app',
+  title: 'Please follow up with Nancy',
+  text: 'Nancy needs a follow up appointment.',
+  data: {
+    patientId: '6',
+  },
+  createdAt: new Date(),
+})
